3c80bf9cff9a5fd7a738c042fa48b899af4f0ce6
Georgios Voutsinas <georgios.voutsinas@desy.de>
2014-07-07 22:26:20 +0000

    Fix for ROOT 5.34.18 - TVector3 Angle fcn

--- src/Criteria/Crit4_3DAngleChangeNormed.cc
+++ src/Criteria/Crit4_3DAngleChangeNormed.cc
@@ -61,8 +61,8 @@ bool Crit4_3DAngleChangeNormed::areCompatible( Segment* parent , Segment* child 
       
       
       
-      double angleXY1 = outerVec.Angle( middleVec.Phi() ); 
-      double angleXY2 = middleVec.Angle( innerVec.Phi() );
+      double angleXY1 = outerVec.Angle( middleVec ); 
+      double angleXY2 = middleVec.Angle( innerVec );
       
       angleXY1 -= 2*M_PI*floor( angleXY1 /2. /M_PI );    //to the range from 0 to 2pi 
       if (angleXY1 > M_PI) angleXY1 -= 2*M_PI;           //to the range from -pi to pi
